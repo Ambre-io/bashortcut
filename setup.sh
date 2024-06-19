8d308fbf82b7f39c6694914444627961ec7f793d
Guillaume Klimek <72322850+guiklimek@users.noreply.github.com>
2024-06-20 00:53:36 +0200

    Paix sur Gaza

--- setup.sh
+++ setup.sh
@@ -8,9 +8,29 @@ cat <<EOF
 
 EOF
 
+
+########################################
+# UTILS
+########################################
+
+ask_exe() {
+    local question="$1"
+    local command="$2"
+
+    read -p "$question (y/n) " -r reply
+    if [[ ${reply} =~ ^[Yy]$ ]]; then
+        eval "$command"
+    fi
+}
+
 ########################################
 # INCLUDE PATHS
 ########################################
+
+cat <<EOF
+# INCLUDE PATHS
+EOF
+
 SETUP=$(command -v -- "${0}")
 SETUPPATH=$(cd -P -- "$(dirname -- "${SETUP}")" && pwd -P)
 LINUXPATHS="${SETUPPATH}/linux/paths"
@@ -20,30 +40,90 @@ LINUXPATHS="${SETUPPATH}/linux/paths"
 . "${LINUXPATHS}"
 
 ########################################
-# INSTALL UTILS
+# CUSTOMIZE OS
 ########################################
 
-[ ! -x "$(command -v tmux)" ] && read -p "Do you want tmux? (y/n) " -r REPLY && [[ ${REPLY} =~ ^[Yy]$ ]] && sudo apt install tmux
-[ ! -x "$(command -v git)" ] && read -p "Do you want git? (y/n) " -r REPLY && [[ ${REPLY} =~ ^[Yy]$ ]] && sudo apt install git && git config --global --add safe.directory "${BASHORTCUT}"
+cat <<EOF
+# CUSTOMIZE OS (Ubuntu Budgie 24.04 gnome-like settings)
+
+EOF
+
+ask_exe "- Reduce Mouse Speed?" "gsettings set org.gnome.desktop.peripherals.mouse speed -0.6"
+
+ask_exe "- Activate Over-Amplification?" "gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true"
+
+ask_exe "- Create App/ folder?" 'mkdir ~/App'
+
+ask_exe "- Pin App/ folder in file explorer?" "[ ! -d \"~/App\" ] && sed -i '1s/^/file:\/\/\/${HOME}\/App App\n/' ~/.config/gtk-3.0/bookmarks"
+
+ask_exe "- Switch Power Mode to Performance?" "powerprofilesctl set performance"
+
+ask_exe "- Deactivate Screen Blank?" "gsettings set org.gnome.desktop.session idle-delay 0"
+
+ask_exe "- Deactivate Automatic Power Saver?" "gsettings set org.gnome.settings-daemon.plugins.power power-saver-profile-on-low-battery false"
+
+ask_exe "- Deactivate Automatic Suspend?" "gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing' && gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'"
+
+ask_exe "- Deactivate Power Button Behavior?" "gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'nothing'"
+
+ask_exe "- Deactivate Automatic Screen Lock?" "gsettings set org.gnome.desktop.screensaver lock-enabled false"
+
+ask_exe "- Deactive Unicode Code Point ctrl+shift+u?" 'sudo apt install ibus && gsettings set org.freedesktop.ibus.panel.emoji unicode-hotkey "@as []"'
+
+########################################
+# INSTALL TOOLS
+########################################
+
+cat <<EOF
+# INSTALL TOOLS
+
+EOF
+
+# tmux
+if [ ! -x "$(command -v tmux)" ]; then
+	read -p "Do you want tmux? (y/n) " -r REPLY 
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+		sudo apt install tmux
+	fi
+fi
+
+# git
+if [ ! -x "$(command -v git)" ]; then
+	read -p "Do you want git? [y/n] " -r REPLY
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+		sudo apt install git
+		git config --global --add safe.directory "${BASHORTCUT}"
+	fi
+fi
 # TODO
 #   - git config --global alias.co checkout
 #   - git config --global credential.helper cache
-[ ! -x "$(command -v gedit)" ] && read -p "Do you want gedit? (y/n) " -r REPLY && [[ ${REPLY} =~ ^[Yy]$ ]] && sudo apt install gedit
 
-########################################
-# INSTALL DOCKER
-########################################
+# gedit
+if [ ! -x "$(command -v gedit)" ]; then
+	read -p "Do you want gedit? (y/n) " -r REPLY
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+		sudo apt install gedit
+	fi
+fi
+
+# Docker
 read -p "Do you want docker? (y/n) " -r REPLY
 if [[ ${REPLY} =~ ^[Yy]$ ]] && ! systemctl is-active --quiet docker; then
+
+	# Add Docker's official GPG key
     echo "Docker Engine repository preparation & installation"
-    echo "Maybe you should look if something change here: https://docs.docker.com/engine/install/ubuntu/"
     sudo apt-get install ca-certificates curl gnupg
     sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-        "deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        \"$(. /etc/os-release && echo "$VERSION_CODENAME")\" stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+	sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources
+	echo \
+		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+		$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+		sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
     if { sudo apt-get update 2>&1 || echo E: update failed; } | grep -q '^[WE]:'; then
         sudo chmod a+r /etc/apt/keyrings/docker.gpg
         sudo apt-get update
@@ -51,15 +131,6 @@ if [[ ${REPLY} =~ ^[Yy]$ ]] && ! systemctl is-active --quiet docker; then
 
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo usermod -aG docker "${USER}"
-fi
-
-########################################
-# Disable unicode code shortcut
-########################################
-# see: https://superuser.com/a/1392682
-read -p "Do you want to disable the unicode shortcut CTRL+SHIFT+u on Ubuntu? (y/n) " -r REPLY
-if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-    gsettings set org.freedesktop.ibus.panel.emoji unicode-hotkey "@as []"
 fi
 
 ########################################
