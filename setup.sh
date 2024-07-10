e915a719c17320ee086d9eb53ef093424e40bf23
Guillaume Klimek <72322850+guiklimek@users.noreply.github.com>
2024-07-10 03:44:55 +0200

    Paix | Customise Dock

--- setup.sh
+++ setup.sh
@@ -111,6 +111,26 @@ fi
 [ ! -f "${HOME}/notes" ] && ask_exe "- Create a notes file?" "touch ${HOME}/notes"
 
 ########################################
+# CUSTOMIZE DOCK
+########################################
+
+read -p "- Do you want to customize the Dock? [Y/n] " -r reply
+if [[ ${reply} =~ ^[Yy]$ ]]; then
+
+	ask_exe "- Position Dock to the left?" "gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ position 'left'"
+
+	ask_exe "- Delete LibreOffice dings?" "rm ${HOME}/.config/plank/dock1/launchers/libreoffice*.dockitem"
+
+	ask_exe "- Delete Budgie Welcome dings?" "rm ${HOME}/.config/plank/dock1/launchers/budgie-welcome.dockitem"
+	
+	ask_exe "- Icon Size 42?" "gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ icon-size 42"
+	
+	ask_exe "- Icon Zoom 130?" "gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ icon-zoom 130"
+
+	ask_exe "- Disable Auto-Hide?" "gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ autohide false"
+fi
+
+########################################
 # INSTALL TOOLS
 ########################################
 
