ac5e2e9752a243eb7a0f215ef5aef970b3f4f4db
guiklimek <guillaume.klimek@tutanota.com>
2025-09-12 16:00:16 +0200

    gagst => gaa, shortcuted multifiles

--- commands/shortcuted.sh
+++ commands/shortcuted.sh
@@ -1,14 +1,19 @@
-#! /bin/bash -e
+#!/bin/bash -e
 
-if [ ${#} -lt 1 ]; then
-    echo "Use: shortcuted <filepath>"
-    echo "Example: shortcuted ./file"
-else
-	filepath=$(realpath "${1}")
-	if [ ! -f "${filepath}" ]; then
-		echo "Error: please check the path: ${filepath}"
-		exit 1
-	fi
-	sudo ln -s "${filepath}" "/usr/local/bin"
-	echo "$(basename "${filepath}") shortcuted!"
+if [ $# -lt 1 ]; then
+    echo "Use: shortcuted <filepath1> [filepath2 ...]"
+    echo "Example: shortcuted ./file1 ./file2"
+    exit 1
 fi
+
+for filepath in "$@"; do
+    fullpath=$(realpath "$filepath")
+
+    if [ ! -f "$fullpath" ]; then
+        echo "Error: please check the path: $fullpath"
+        continue
+    fi
+
+    sudo ln -s "$fullpath" "/usr/local/bin/$(basename "$fullpath")"
+    echo "$(basename "$fullpath") shortcuted!"
+done
