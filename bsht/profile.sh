29ac9c3003ecf068f4213f92612760ca70659fad
guiklimek <guillaume.klimek@tutanota.com>
2025-09-12 19:02:33 +0200

    go bin!

--- bsht/profile.sh
+++ bsht/profile.sh
@@ -51,5 +51,4 @@ export NVM_DIR="${HOME}/.nvm"
 [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
 
 # Go
-[ -d "/usr/local/go/bin" ] && export PATH=${PATH}:/usr/local/go/bin:${HOME}/go
-
+[ -d "/usr/local/go/bin" ] && export PATH="${PATH}:/usr/local/go/bin:${HOME}/go:${HOME}/go/bin"
